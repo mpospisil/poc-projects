c9f87ba5bf8433560338d5229d5507e39dd328e2
Martin Pospisil <martin.pospisil@ideastatica.com>
2024-07-25 17:21:46 +0200

    webui::window my_window;

--- WebUI-CL/ConLibClient/ConLibUI/ConLibFunctions.cpp
+++ WebUI-CL/ConLibClient/ConLibUI/ConLibFunctions.cpp
@@ -1,9 +1,14 @@
 #include "pch.h"
 #include "ConLibFunctions.h"
 #include <iostream>
+#include "webui.hpp"
 
 extern "C" CONLIBUI_DLL_API const wchar_t* GetPropose(const wchar_t* pSearchArg)
 {
+	webui::window my_window;
+	my_window.show("<html><head><script src=\"webui.js\"></script></head> C++ Hello World ! </html>");
+	webui::wait();
+
   std::wstring* result = new std::wstring(L"Proposed String");
   return result->c_str();
 }

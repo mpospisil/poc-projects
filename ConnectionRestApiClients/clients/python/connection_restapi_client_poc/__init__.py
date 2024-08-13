ad6f80f313f2c33fdc0b4d98d29fe20069d17fcc
Martin Pospisil <martin.pospisil@ideastatica.com>
2024-08-13 17:07:14 +0200

    v24.0.5.0417

--- ConnectionRestApiClients/clients/python/connection_restapi_client_poc/__init__.py
+++ ConnectionRestApiClients/clients/python/connection_restapi_client_poc/__init__.py
@@ -14,7 +14,7 @@
 """  # noqa: E501
 
 
-__version__ = "24.0.5.0406"
+__version__ = "24.0.5.0417"
 
 # import apis into sdk package
 from connection_restapi_client_poc.api.calculation_api import CalculationApi

3e4fc6ddaa723e6dff53851b94dd5916142cfd08
Martin Pospisil <martin.pospisil@ideastatica.com>
2024-08-19 21:25:50 +0200

    formatting

--- WebApiTest/WebApiService/Controllers/ServiceApiController.cs
+++ WebApiTest/WebApiService/Controllers/ServiceApiController.cs
@@ -3,19 +3,19 @@ using Microsoft.AspNetCore.Mvc;
 namespace MyApi.Controllers
 {
 	[ApiController]
-    [Route("api/[controller]")]
-    public class ServiceApiController : ControllerBase
-    {
-        /// <summary>
-        /// Gets a list of values.
-        /// </summary>
-        /// <returns>A list of strings.</returns>
-        [HttpGet]
-        [ProducesResponseType(typeof(IEnumerable<string>), 200)]
-        public IActionResult GetValues()
-        {
-            var values = new List<string> { "value1", "value2" };
-            return Ok(values);
-        }
-    }
+	[Route("api/[controller]")]
+	public class ServiceApiController : ControllerBase
+	{
+		/// <summary>
+		/// Gets a list of values.
+		/// </summary>
+		/// <returns>A list of strings.</returns>
+		[HttpGet]
+		[ProducesResponseType(typeof(IEnumerable<string>), 200)]
+		public IActionResult GetValues()
+		{
+			var values = new List<string> { "value1", "value2" };
+			return Ok(values);
+		}
+	}
 }

e76324e57de9d730f927cc0378613dcbacdf8b89
Martin Pospisil <martin.pospisil@ideastatica.com>
2024-09-30 17:25:00 +0200

    IdeaStatiCaClient

---
+++ ConnectionRestApiClients/clients/python/connection_restapi_client_poc/ideastatica_client.py
@@ -0,0 +1,57 @@
+from typing import Optional
+import os
+import connection_restapi_client_poc 
+import connection_restapi_client_poc.api_client as api_client
+
+class IdeaStatiCaClient:
+    def __init__(self, configuration : connection_restapi_client_poc.Configuration, fileName : str):
+        self.fileName = fileName
+        self.configuration = configuration
+        self.client: Optional[api_client.ApiClient] = None
+        self.project_id: Optional[str] = None
+        self.client_id: Optional[str] = None
+        self.api_project: Optional[connection_restapi_client_poc.ProjectApi] = None
+
+    def __enter__(self):
+        with open(self.fileName, 'rb') as file:
+            byte_array = file.read()
+
+        # Initialize the client with the provided config
+        self.client = api_client.ApiClient(self.configuration)
+
+        client_api = connection_restapi_client_poc.ClientApi(self.client)
+        self.client_id = client_api.connect_client()
+
+        # Add your ClientId to HTTP header
+        self.client.default_headers['ClientId'] = self.client_id        
+
+        self.api_project = connection_restapi_client_poc.ProjectApi(self.client)
+
+        # Override the default Content-Type for this specific call
+        uploadRes = self.api_project.open_project(idea_con_file=byte_array, _content_type='multipart/form-data')
+        self.project_id = uploadRes.project_id
+
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        # Perform any necessary cleanup
+        try:
+            if self.api_project:
+                self.api_project.close_project(self.project_id)
+                self.api_project = None
+        finally:
+            self.api_project = None    
+
+
+def OpenClient(configuration: connection_restapi_client_poc.Configuration , fileName: str):
+    """
+    Opens a client connection to the IdeaStatiCa API.
+
+    Args:
+        configuration (connection_restapi_client_poc.Configuration): The configuration object for the client.
+        fileName (str): The name of the file to be opened.
+
+    Returns:
+        IdeaStatiCaClient: An instance of the IdeaStatiCaClient class representing the client connection.
+    """
+    return IdeaStatiCaClient(configuration, fileName)

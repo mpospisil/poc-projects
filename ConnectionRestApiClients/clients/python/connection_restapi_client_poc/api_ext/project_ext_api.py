2af4f91c2db4d331ade9bf33d7bae5a175cf15f5
Martin Pospisil <martin.pospisil@ideastatica.com>
2024-10-04 10:53:01 +0200

    ideastatica_client - all controllers

--- ConnectionRestApiClients/clients/python/connection_restapi_client_poc/api_ext/project_ext_api.py
+++ ConnectionRestApiClients/clients/python/connection_restapi_client_poc/api_ext/project_ext_api.py
@@ -1,4 +1,7 @@
+import logging
 from connection_restapi_client_poc.api.project_api import ProjectApi
+
+logger = logging.getLogger(__name__)
 
 class ProjectExtApi(ProjectApi):
     def __init__(self, *args, **kwargs):

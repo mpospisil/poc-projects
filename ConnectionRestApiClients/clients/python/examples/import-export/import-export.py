f8414d60c1e26ba4a5156dd4df3c7bc4a0018695
Martin Pospisil <martin.pospisil@ideastatica.com>
2024-10-04 08:21:20 +0200

    import-export.py

---
+++ ConnectionRestApiClients/clients/python/examples/import-export/import-export.py
@@ -0,0 +1,39 @@
+import sys
+import os
+import json
+from pprint import pprint
+from urllib.parse import urljoin
+
+# Get the parent directory
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+
+# Add the parent directory to sys.path
+sys.path.append(parent_dir)
+
+import connection_restapi_client_poc
+import connection_restapi_client_poc.ideastatica_client as ideastatica_client
+
+baseUrl = "http://localhost:5000"
+
+# Defining the host is optional and defaults to http://localhost
+# See configuration.py for a list of all supported configuration parameters.
+configuration = connection_restapi_client_poc.Configuration(
+    host = baseUrl
+)
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+project_file_path = os.path.join(dir_path, '..\projects', 'HSS_norm_cond.ideaCon')
+print(project_file_path)
+
+
+# Enter a context with an instance of the API client
+with ideastatica_client.IdeaStatiCaClient(configuration, project_file_path) as is_client:
+
+    try:
+        # Get the project data
+        project_data = is_client.project.get_project_data(is_client.project_id)
+        pprint(project_data)
+
+
+    except Exception as e:
+        print("Operation failed : %s\n" % e)

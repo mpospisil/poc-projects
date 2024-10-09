2befcd726b8fa2661befd1446032c21545dee566
nathanlukeIDEA <nathan.luke@ideastatica.com>
2024-10-09 15:40:10 +1100

    Add generate report and update load effect c# examples
    Add generate report and update load effect c# examples

---
+++ ConnectionRestApiClients/clients/csharp/examples/GenerateReport/Program.cs
@@ -0,0 +1,48 @@
+﻿using connection_restapi_client_poc;
+using connection_restapi_client_poc.Api;
+using connection_restapi_client_poc.Model;
+
+namespace GenerateReport
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            ConnectionApiClientFactory clientFactory = new ConnectionApiClientFactory("http://localhost:5000");
+            
+            ConnectionApiClient conclient = (ConnectionApiClient)await clientFactory.CreateConnectionApiClient();
+
+            try
+            {
+                string filePath = "inputs/HSS_norm_cond.ideaCon";
+                ConProject conProject = await conclient.Project.OpenProjectFromFileAsync(filePath);
+
+                //Get projectId Guid
+                Guid projectId = conProject.ProjectId;
+                var connections = await conclient.Connection.GetAllConnectionsDataAsync(projectId);
+                int connectionId = connections[0].Id;
+
+                //TO DO - FIX REPORT GENERATION
+                //Generate Report
+                //conclient.Report.GeneratePdfAsync()
+
+
+                //Change Member Size
+
+
+
+                //Generate Report Again
+
+
+
+                string saveFilePath = "member-size-update.ideaCon";
+
+                await conclient.Project.SaveProjectAsync(projectId, saveFilePath);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}

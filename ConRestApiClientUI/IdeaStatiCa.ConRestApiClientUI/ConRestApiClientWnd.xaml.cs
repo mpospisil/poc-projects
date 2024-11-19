e8cd7463421a783bf7154a96bdc628036202edf6
Martin Pospisil <martin.pospisil@ideastatica.com>
2024-11-19 09:39:51 +0100

    index.html

--- ConRestApiClientUI/IdeaStatiCa.ConRestApiClientUI/ConRestApiClientWnd.xaml.cs
+++ ConRestApiClientUI/IdeaStatiCa.ConRestApiClientUI/ConRestApiClientWnd.xaml.cs
@@ -19,7 +19,24 @@ namespace IdeaStatiCa.ConRestApiClientUI
 		{
 			await webView.EnsureCoreWebView2Async(null);
 
-			string htmlFilePath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "static", "test1.html");
+			string htmlFilePath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "static", "index.html");
+			//string htmlFilePath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "static2", "index.html");
+
+			//// Disable CORS
+			//webView.CoreWebView2.WebResourceRequested += (sender, args) =>
+			//{
+			//	var request = args.Request;
+			//	var headers = request.Headers;
+
+			//	// Add headers to disable CORS
+			//	headers.SetHeader("Access-Control-Allow-Origin", "*");
+			//	headers.SetHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+			//	headers.SetHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+			//	args.Response = webView.CoreWebView2.Environment.CreateWebResourceResponse(
+			//			null, 200, "OK", headers.ToString());
+			//};
+
 			webView.CoreWebView2.Navigate(new Uri(htmlFilePath).AbsoluteUri);
 
 			var host = new ClientHost();

2d8d571e98ab96afaff3abb822156129115ca409
Martin Pospisil <martin.pospisil@ideastatica.com>
2025-06-06 11:38:59 +0200

    project

---
+++ webgl-presenter/WebGlPresenterApp/WebGlPresenterApi.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+namespace WebGlPresenterApp
+{
+	public class WebGlPresenterApi
+	{
+		[DllImport("WebGlPresenter.dll", CharSet = CharSet.Ansi)]
+		public static extern void ShowScene(string searchArg);
+	}
+}
